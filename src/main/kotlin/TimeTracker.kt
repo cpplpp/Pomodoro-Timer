04f685598141c7bb48db11c3182a396895193167
MorozovGl <morozovgleb1235@yandex.ru>
2019-01-16 23:03:32 +0300

    Change implementation from status bar to tool window

--- src/main/kotlin/TimeTracker.kt
+++ src/main/kotlin/TimeTracker.kt
@@ -1,6 +1,9 @@
-import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
 
-class TimeTracker(private var project: Project) : ProjectComponent {
+class TimeTracker: ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
 
+    }
 }
